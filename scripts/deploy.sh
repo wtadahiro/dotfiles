8f43b9a9e8ce9a86f5cc3fea89e89b5f3371fa42
tadahiro.wada <tadahiro.wada@d8aspring.com>
2017-05-01 19:52:06 +0900

    add deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+base_path=`pwd`;
+ignores=('.' '..' '.git' '.gitignore' '.gitmodules')
+
+isIgnored()
+{
+  local e
+  for e in ${ignores[@]}; do [[ "$e" == "$1" ]] && return 0; done
+  return 1
+}
+
+for file_path in $base_path/.*; do
+  filename=`basename $file_path`
+  isIgnored $filename
+  if [ $? -eq 1 ]; then
+    ln -sf $file_path $HOME/$filename
+  fi
+done
+
